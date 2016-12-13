7c08292403eecb5f42c48da23d13cdf6cdf10467
xiongxin <xiongxin@e-shion.net>
2016-12-13 15:02:52 +0800

    Initial commit

---
+++ app/src/main/java/com/xx/togglebutton/MainActivity.java
@@ -0,0 +1,17 @@
+package com.xx.togglebutton;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+/**
+ * Created by eshion on 16-10-14.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+    }
+}
